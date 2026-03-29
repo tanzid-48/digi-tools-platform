b22fb4334fbf24b601c94ff41034b9bd47bf14f5
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-29 21:36:18 +0600

    Project Setup and add google font-Manrope tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    <div>
+   
+      <h1 className='text-3xl text-red-500'>Get started</h1>
+      <button className="btn btn-accent">Accent</button>
+
+    </div>
+
+
+  )
+}
+export default App
